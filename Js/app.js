94567d2e88dde667b27c0eb54f12231e9dd757d4
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2021-11-13 17:14:56 -0300

    Feita todas às páginas de login juntamento com o responsivo

--- Js/app.js
+++ Js/app.js
@@ -71,3 +71,15 @@ function fecharMenu(){
 }
 
 //============================================================================//
+
+function getPassword() {
+	var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]";
+	var passwordLength = 16;
+	var password = "";
+
+	for (var i = 0; i < passwordLength; i++) {
+	  var randomNumber = Math.floor(Math.random() * chars.length);
+	  password += chars.substring(randomNumber, randomNumber + 1);
+	}
+	document.getElementById('password').value = password
+}
