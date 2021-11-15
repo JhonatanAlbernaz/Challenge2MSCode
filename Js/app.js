13665ae5db148c053202ef5a32ef277d821b341e
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2021-11-15 17:54:36 -0300

    Feito a página sobre nós juntamente com o responsivo

--- Js/app.js
+++ Js/app.js
@@ -194,3 +194,29 @@ function emailVerificado(valor_input_email) {
 }
 
 //============================================================================//
+
+$(document).ready(function(){
+
+    $(".counter").each(function() {
+      var $this = $(this),
+  
+          countTo = $this.attr("data-count");
+  
+      $({ countNum: $this.text()}).animate({
+        countNum: countTo
+      },
+    
+      {
+        duration: 4000,
+        easing:"linear",
+        step: function() {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+          $this.text(this.countNum);
+        }
+      });
+    });
+});
+
+//============================================================================//
