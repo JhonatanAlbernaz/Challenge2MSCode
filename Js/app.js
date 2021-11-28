f26beaf6607f1322a084f8f5ae92fef801a16210
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2021-11-28 13:50:25 -0300

    Finalizado o Challenge 2

--- Js/app.js
+++ Js/app.js
@@ -425,7 +425,6 @@ google.charts.load('current', {'packages':['corechart']});
 var closebtns = document.getElementsByClassName("close");
 var i;
 
-/* Loop through the elements, and hide the parent, when clicked on */
 for (i = 0; i < closebtns.length; i++) {
   closebtns[i].addEventListener("click", function() {
     this.parentElement.style.display = 'none';
